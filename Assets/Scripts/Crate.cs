74042fccb8a5690fe80284dde68f7641d5f3d472
khaos <naether.markus@racoonstudios.de>
2015-01-11 15:45:16 +0100

    Added glowing materials and animation with sound effects for open the crate.

---
+++ Assets/Scripts/Crate.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crate : MonoBehaviour {
+
+	public bool bIsOpen;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!this.bIsOpen) {
+			this.OpenFront();
+		}
+	}
+
+	void OpenFront() {
+		// Play animation for open the front door
+		this.animation.Play ("CratePlateOpen");
+
+		// Play sound
+		this.audio.Play ();
+
+		this.bIsOpen = true;
+	}
+}
